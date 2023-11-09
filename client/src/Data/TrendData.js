bbb48914fdb1f1f699b016d92d9aed16707c56b5
DeepanshuGoel1122 <deepanshugoel1122@gmail.com>
2023-11-09 13:15:38 +0530

    first commit

---
+++ client/src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData= [
+  {
+    name: "React",
+    shares: 97,
+  },
+  {
+    name: "MERN",
+    shares: 80.5,
+  },
+  {
+    name: "MongoDB",
+    shares: 75.5,
+  },
+  {
+    name: "Reactjs",
+    shares: 72,
+  },
+  {
+    name: "Elon Musk",
+    shares: 71.9,
+  },
+  {
+    name: "Social App",
+    shares: 20,
+  },
+];
